365c4419d8f8afd555e07c09be7aabbcd390493a
emildsn <emildsn00@gmail.com>
2023-05-30 18:09:33 -0300

    Initial Commit

---
+++ task-app-ios/task_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_app_iosApp.swift
+//  task-app-ios
+//
+//  Created by Emiliano Diaz on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct task_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
